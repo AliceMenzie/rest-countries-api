ce5580f955c136e87324830d7cd2811d311d05e6
AliceMenzie <alice.menzie@gmail.com>
2021-11-03 09:15:40 +1100

    add dark mode icon and text component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BsMoon } from 'react-icons/bs';
+
 
 export default function Navbar() {
   return (
     <nav>
-      <Link to={`/`}>
+      <Link className="btn-home" to={`/`}>
         <h3>Where in the World?</h3>
       </Link>
+      <div className="theme">
+      < BsMoon/>
+      <p>Dark Mode</p>
+      </div>
     </nav>
   );
 }
