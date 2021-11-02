fecc7411b305cedea53b26618351560e66edf0ee
AliceMenzie <alice.menzie@gmail.com>
2021-11-03 09:14:17 +1100

    add content to countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,21 +3,26 @@ import { Link } from "react-router-dom";
 
 export default function Countries({ filteredCountries }) {
   return (
-    <div>
+    <div className="countries-container" >
+      
       {filteredCountries.map((country) => (
         
-        <div key={country.alpha3Code}>
-          <Link  to={`country/${country.name}`}>
-            <img className="flag" alt='flag' src={country.flag}/>
 
-            <h3> {country.name} </h3>
+        <div key={country.alpha3Code} className="country">
+          <Link  to={`country/${country.name}`}>
+            <img className="flag" alt='flag' src={country.flag}/>  
           </Link>
-         
-          <p> {country.region} </p>
-          <p> {country.capital} </p>
-          <p> {country.population.toLocaleString()} </p>
+          <div className="country-contents">
+          <h3> {country.name} </h3>
+          <p><strong>Population: </strong> {country.population.toLocaleString()} </p>
+          <p><strong>Region: </strong> {country.region}  </p>
+          <p><strong>Capital: </strong> {country.capital}  </p>
+          </div>
         </div>
+        
       ))}
+      
+      
     </div>
   );
 }
