564b7fb78e4830acef80563832ce3a8de0ccc845
AliceMenzie <alice.menzie@gmail.com>
2021-11-08 14:41:11 +1100

    update formatting and styling

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -9,16 +9,18 @@ export default function Countries({ filteredCountries }) {
         
 
         <div key={country.alpha3Code} className="country">
+          <div className="img-wrapper">
           <Link  to={`country/${country.name}`}>
             <img className="flag" alt='flag' src={country.flag}/>  
           </Link>
+          </div>
           <div className="country-contents">
           <h3> {country.name} </h3>
           <p><strong>Population: </strong> {country.population.toLocaleString()} </p>
           <p><strong>Region: </strong> {country.region}  </p>
           <p><strong>Capital: </strong> {country.capital}  </p>
           </div>
-        </div>
+         </div>
         
       ))}
       
